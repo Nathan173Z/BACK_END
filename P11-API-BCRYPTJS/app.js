3a0cf554231f0524caa8954fb460048b287e344a
NATANAEL PEREIRA DOS SANTOS <natanael.psantos1@senacsp.edu.br>
2022-06-27 22:07:42 -0300

    P11 enviando email

--- P11-API-BCRYPTJS/app.js
+++ P11-API-BCRYPTJS/app.js
@@ -3,9 +3,7 @@ const bcrypt = require('bcryptjs');
 const app = express();
 const User = require('./models/User');
 require('dotenv').config();
-
-
-
+const sendMail = require('./providers/mailProvider');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
@@ -60,19 +58,45 @@ app.get('/users/:id', async (req, res) =>{
     }
 })
 
-app.post("/user", async (req, res) =>{
-    // const {name, email, gender, password} = req.body;
+app.post("/user", async (req, res) => {
+
     var dados = req.body;
-    console.log(dados);
     dados.password = await bcrypt.hash(dados.password, 8);
-    console.log(dados.password);
+    let email = dados.email;
+    let name = dados.name;
+    let gender = dados.gender;
 
-
-    await User.create(req.body)
+    await User.create(dados)
     .then( ()=>{
+        /* enviar e-mail */
+        let to = email;
+        let cc = '';
+        var htmlbody = "";
+        htmlbody += '<div style="background-color:#000; margin-bottom:150px;">';
+        htmlbody += '<div style="margin-top:150px;">';
+        htmlbody += '<p style="color:#fff; font-weight:bold;margin-top:50px;">';
+        htmlbody += 'Olá {name},';
+        htmlbody += '</p>';
+        htmlbody += '<p style="color:#fff; font-style:italic;margin-top:50px;">';
+        htmlbody += 'Sua conta foi criada com sucesso!';
+        htmlbody += '</p>';
+        htmlbody += '<p style="color:#fff;margin-top:50px;">';
+        htmlbody += 'Seu login é o seu email: {email}';
+        htmlbody += '</p>';
+        htmlbody += '<p style="color:#fff;margin-top:50px;">';
+        htmlbody += 'Sexo: {gender}';
+        htmlbody += '</p>';
+        htmlbody += '</div>';
+        htmlbody += '</div>';
+        htmlbody = htmlbody.replace('{name}', name);
+        htmlbody = htmlbody.replace('{email}', email);
+        htmlbody = htmlbody.replace('{gender}', gender);
+        /* ************* */
+        sendMail(to, cc, 'Sua conta foi criada com sucesso!', htmlbody);
+
         return res.json({
-            erro:false,
-            mensagem: 'Usuário cadastrado com sucesso'
+            erro: false,
+            mensagem: 'Usuário cadastrado com sucesso!'
         });
     }).catch( (err)=>{
         return res.status(400).json({
@@ -99,7 +123,7 @@ app.put("/user", async (req, res) =>{
     })
 })
     
-    app.delete("user/:id", async (req, res) => {
+    app.delete("/user/:id", async (req, res) => {
         const {id} = req.params;
         await User.destroy({ where: {id}})
         .then( () => {
